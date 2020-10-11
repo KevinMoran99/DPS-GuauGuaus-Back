619d702ec390cea829b27247d607e000c6af0a61
German <caserspinosa@yahoo.com>
2020-10-11 11:26:52 -0600

    rutas para appointmenttypes, medical conditions, permissions, user types y users

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ $router->get('/', function () use ($router) {
 
 });
 //$router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->get('/users', 'UserController@index');
-    $router->get('/species', 'SpecieController@index');
-
+    
+    /*routes pets*/
     $router->get('/pets', 'PetController@index');
     $router->post('/pets', 'PetController@store');
     $router->get('/pets/{id}', 'PetController@show');
@@ -26,8 +25,40 @@ $router->get('/', function () use ($router) {
     $router->get('/pets/client/{owner_id}', 'PetController@getPetbyOwner');
     $router->get('/pets/specie/{specie_id}', 'PetController@getPetbySpecie');
 
-
+    /*routes species*/
+    $router->get('/species', 'SpecieController@index');
     $router->get('/species/{id}', 'SpecieController@show');
     $router->post('/species', 'SpecieController@store');
     $router->put('/species', 'SpecieController@update');
+
+
+    /*routes User Types*/
+    $router->get('/usertypes', 'UserType@index');
+    $router->get('/usertypes/{id}', 'UserType@show');
+    $router->post('/usertypes', 'UserType@store');
+    $router->put('/usertypes', 'UserType@update');
+
+    /*routes Permissions*/
+    $router->get('/permissions', 'Permission@index');
+    $router->get('/permissions/{id}', 'Permission@show');
+    $router->post('/permissions', 'Permission@store');
+    $router->put('/permissions', 'Permission@update');
+
+    /*routes Medical Conditions*/
+    $router->get('/medicalconditions', 'MedicalCondition@index');
+    $router->get('/medicalconditions/{id}', 'MedicalCondition@show');
+    $router->post('/medicalconditions', 'MedicalCondition@store');
+    $router->put('/medicalconditions', 'MedicalCondition@update');
+
+    /*routes Appointment Types*/
+    $router->get('/appointmenttypes', 'AppointmentType@index');
+    $router->get('/appointmenttypes/{id}', 'AppointmentType@show');
+    $router->post('/appointmenttypes', 'AppointmentType@store');
+    $router->put('/appointmenttypes', 'AppointmentType@update');
+
+    /*routes User*/
+    $router->get('/users', 'User@index');
+    $router->get('/users/{id}', 'User@show');
+    $router->post('/users', 'User@store');
+    $router->put('/users', 'User@update');
 //});
